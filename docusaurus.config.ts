1dc7237b17de6235aa21d4d88dbcab9b1349c3a4
Satsumaage <warabi.0719@gmail.com>
2025-03-31 01:18:09 +0900

    change img

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'FxxkUnityDocs',
   tagline: 'Unityなんもわからん',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/Ufavicon.ico',
 
   // Set the production url of your site here
   url: 'https://satsumaage.github.io',
